2e02cff0ce371eed7fa65ec685a5175b3fae8ce3
Aadith A <88674824+aadithatg@users.noreply.github.com>
2021-08-21 09:49:39 +0100

    Add files via upload

---
+++ pro-c10-sa1-template-main/sketch.js
@@ -0,0 +1,18 @@
+var weight =[20,25,40,45];
+function avgweight() {
+  var sum = weight[0] + weight[1] + weight[2] + weight[3];
+  var avg = sum/weight.length
+  console.log(avg)
+}
+function setup() 
+{
+  createCanvas(400,400);
+  avgweight()
+}
+
+function draw() 
+{
+background(51);
+
+}
+
